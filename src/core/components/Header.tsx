ecaf23e642278829d6ce1211d719b17268ea4372
Eugen Neufeld <eneufeld@eclipsesource.com>
2020-05-27 17:41:23 +0200

    Add Export dialog for schema and uiSchema
    - Add an export dialog to show schema and uischema
    - Add download button to header
    
    Fixes #17
    
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

--- src/core/components/Header.tsx
+++ src/core/components/Header.tsx
@@ -6,16 +6,59 @@
  * ---------------------------------------------------------------------
  */
 import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import React from 'react';
+import CloudDownload from '@material-ui/icons/CloudDownload';
+import React, { useState } from 'react';
 
-export const Header: React.FC = () => (
-  <AppBar position='static' elevation={0}>
-    <Toolbar>
-      <Typography variant='h6' color='inherit' noWrap>
-        JSON Forms Editor
-      </Typography>
-    </Toolbar>
-  </AppBar>
+import { useExportSchema, useExportUiSchema } from '../util/hooks';
+import { ExportDialog } from './ExportDialog';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    title: {
+      flexGrow: 1,
+    },
+  })
 );
+
+export const Header: React.FC = () => {
+  const classes = useStyles();
+  const schema = useExportSchema();
+  const uiSchema = useExportUiSchema();
+  const [open, setOpen] = useState(false);
+  const onClose = () => setOpen(false);
+  const openDownloadDialog = () => setOpen(true);
+
+  return (
+    <AppBar position='static' elevation={0}>
+      <Toolbar>
+        <Typography
+          variant='h6'
+          color='inherit'
+          noWrap
+          className={classes.title}
+        >
+          JSON Forms Editor
+        </Typography>
+        <IconButton
+          aria-label={`Download`}
+          onClick={openDownloadDialog}
+          color='inherit'
+        >
+          <CloudDownload />
+        </IconButton>
+      </Toolbar>
+      {open && (
+        <ExportDialog
+          open={open}
+          onClose={onClose}
+          schema={schema}
+          uiSchema={uiSchema}
+        />
+      )}
+    </AppBar>
+  );
+};
