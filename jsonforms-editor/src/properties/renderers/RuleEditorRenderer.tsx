951911c0627cd230e549e20d648fccda254beb30
Alexandra Buzila <abuzila@eclipsesource.com>
2021-01-21 13:31:02 +0100

    Improve Rule UX (#119)
    - add collapsible control for rule. The rule is collapsed by default
    when no rule definition exists
    - add description and small example for rule
    
    Fixes #118

--- jsonforms-editor/src/properties/renderers/RuleEditorRenderer.tsx
+++ jsonforms-editor/src/properties/renderers/RuleEditorRenderer.tsx
@@ -8,12 +8,24 @@
 
 import { ControlProps, rankWith, scopeEndsWith } from '@jsonforms/core';
 import { withJsonFormsControlProps } from '@jsonforms/react';
-import { Button, FormHelperText, Grid, Typography } from '@material-ui/core';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  FormHelperText,
+  Grid,
+  makeStyles,
+  Theme,
+  Typography,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Uri } from 'monaco-editor/esm/vs/editor/editor.api';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import React, { useCallback, useMemo, useState } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 
+import { ShowMoreLess } from '../..';
 import {
   configureRuleSchemaValidation,
   EditorApi,
@@ -21,11 +33,51 @@ import {
 } from '../../text-editor/jsonSchemaValidation';
 
 const invalidJsonMessage = 'Not a valid rule JSON.';
+const ruleDescription =
+  'Define conditions and effects that can dynamically control features of the UI based on data.';
 
+const ruleExample = (
+  <div>
+    <h3>Example</h3>
+    <p>
+      A rule that hides the UI Element it is contained in, when the value of the
+      control with the scope <b>'#/properties/name'</b> is <b>'foo'</b>:
+    </p>
+    <pre>
+      {JSON.stringify(
+        {
+          effect: 'HIDE',
+          condition: {
+            type: 'LEAF',
+            scope: '#/properties/name',
+            expectedValue: 'foo',
+          },
+        },
+        null,
+        2
+      )}
+    </pre>
+    <p>
+      Visit the{'  '}
+      <a href='https://jsonforms.io/docs/uischema/rules'>
+        JSON Forms documentation
+      </a>{' '}
+      for more info.
+    </p>
+  </div>
+);
 const isValidRule = (rule: any) => {
   return !rule || (rule.effect && rule.condition);
 };
 
+const useStyles = makeStyles((theme: Theme) => ({
+  editorRoot: {
+    width: '100%',
+  },
+  showMore: {
+    paddingBottom: theme.spacing(2),
+  },
+}));
 const RuleEditor: React.FC<ControlProps> = (props) => {
   const { data, path, handleChange, errors } = props;
   const [invalidJson, setInvalidJson] = useState(false);
@@ -68,33 +120,45 @@ const RuleEditor: React.FC<ControlProps> = (props) => {
   }, [handleChange, model, path]);
 
   const isValid = errors.length === 0 && !invalidJson;
+  const classes = useStyles();
   return (
-    <div>
-      <Typography>Rule</Typography>
-      <MonacoEditor
-        language='json'
-        editorWillMount={configureEditor}
-        editorDidMount={setModel}
-        height={200}
-        options={{
-          formatOnPaste: true,
-          formatOnType: true,
-          automaticLayout: true,
-        }}
-      />
-      <Grid container direction='row' spacing={2} alignItems='center'>
-        <Grid item>
-          <Button variant='contained' onClick={onSubmitRule}>
-            Apply
-          </Button>
-        </Grid>
-        <Grid item>
-          <FormHelperText error={true} hidden={isValid}>
-            {errors.length !== 0 ? errors : invalidJsonMessage}
-          </FormHelperText>
-        </Grid>
-      </Grid>
-    </div>
+    <Accordion defaultExpanded={!!data}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography>Rule</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <div className={classes.editorRoot}>
+          <FormHelperText error={false}>{ruleDescription}</FormHelperText>
+          <ShowMoreLess className={classes.showMore}>
+            <FormHelperText error={false}>{ruleExample}</FormHelperText>
+          </ShowMoreLess>
+
+          <MonacoEditor
+            language='json'
+            editorWillMount={configureEditor}
+            editorDidMount={setModel}
+            height={200}
+            options={{
+              formatOnPaste: true,
+              formatOnType: true,
+              automaticLayout: true,
+            }}
+          />
+          <Grid container direction='row' spacing={2} alignItems='center'>
+            <Grid item>
+              <Button variant='contained' onClick={onSubmitRule}>
+                Apply
+              </Button>
+            </Grid>
+            <Grid item>
+              <FormHelperText error={true} hidden={isValid}>
+                {errors.length !== 0 ? errors : invalidJsonMessage}
+              </FormHelperText>
+            </Grid>
+          </Grid>
+        </div>
+      </AccordionDetails>
+    </Accordion>
   );
 };
 
